5fbf5b77a942cf0357378eb814455dc4c2a35a1d
amar-nath1 <amarat279955>
2022-12-27 19:42:49 +0530

    SignUp page created

---
+++ src/Pages/AuthPage.js
@@ -0,0 +1,85 @@
+import { Button, Container, Form } from "react-bootstrap"
+
+import { useRef } from "react"
+
+
+const AuthPage=()=>{
+
+    const emailRef=useRef()
+    const passwordRef=useRef()
+    const confPasswordRef=useRef()
+
+    const authFormSubmitHandler=(event)=>{
+        event.preventDefault()
+       const emailInput=emailRef.current.value
+       const passwordInput=passwordRef.current.value
+       const confPasswordInput=confPasswordRef.current.value
+
+        if (passwordInput===confPasswordInput){
+
+            const newUserObj={
+                email:emailInput,
+                password:passwordInput,
+                returnSecureToken:true
+            }
+
+            fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDAm8-U9Z7Kq3Mf5WXNnNnR_94M06T1f_k',
+            
+            {
+                method:'POST',
+                body:JSON.stringify(newUserObj),
+                headers:{
+                    'Content-Type':'application/json'
+                }
+        }
+            
+            ).then((res)=>{
+
+                if (res.ok){
+                    res.json().then((data)=>{
+                        console.log(`${data.email} is now registered`)
+                    })
+                }
+
+                else{
+                    return res.json().then((data)=>{
+                        let errorMessage='Authentication Failed'
+                        if (data && data.error && data.error.message){
+
+                            errorMessage=data.error.message
+                        }
+                        alert(errorMessage)
+                    })
+                }
+
+            })
+
+
+        }
+        else {alert('Password do not match')}
+
+    }
+
+    return (
+        <Container className='border border-secondary p-4 m-4 w-25'>
+            <h2 className="ms-4 mb-4 border-bottom pb-2 border-secondary">Sign Up</h2>
+        <Form onSubmit={authFormSubmitHandler}>
+        
+        <Form.Group className="mb-3"  controlId="formBasicEmail">
+        <Form.Control type='email' placeholder='Enter Email' required ref={emailRef}/>
+        </Form.Group>
+
+        <Form.Group className="mb-3"  controlId="formBasicPassword">
+        <Form.Control type='password' placeholder='Enter Password' required ref={passwordRef}/>
+        </Form.Group>
+        <Form.Group className="mb-3"  controlId="formBasicPassword">
+        <Form.Control type='password' placeholder='Confirm Password' required ref={confPasswordRef}/>
+        </Form.Group>
+        <Button className='w-100' type='submit' variant='success'> Sign Up</Button>
+        </Form>
+        </Container>
+    )
+
+}
+
+export default AuthPage
